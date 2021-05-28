4a513ab59ebbfeaa33fde2dc76e4ea14957468ee
Camille Kuila <kuila@Camilles-MBP.local>
2021-05-27 20:47:51 -0700

    mvp, some heroku

---
+++ python/main.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, redirect
+import uuid
+
+app = Flask(__name__)
+redirect_sets = {}
+
+class RedirectSet:
+    def __init__(self, real_url, fake_url):
+        self.id = str(uuid.uuid4())
+        self.real_url = real_url
+        self.fake_url = fake_url
+
+def is_facebook(r):
+    isCrawler = False
+    userAgent = request.headers['User-Agent']
+    print(userAgent)
+    if 'facebook' in userAgent:
+        isCrawler = True
+
+    return isCrawler
+
+@app.route('/redirects/<id>', methods=['GET'])
+def handle_redirect(id):
+    rs = redirect_sets[id]
+
+    if is_facebook(request):
+        return redirect(rs.fake_url)
+    else:
+        return redirect(rs.real_url)
+
+@app.route('/redirects', methods=['POST'])
+def create_redirect_set():
+    rs = RedirectSet(request.form['real_url'],
+                     request.form['fake_url'])
+    redirect_sets[rs.id] = rs
+    return '''Created RedirectSet.
+           ID is %s
+           Real URL: %s
+           Fake URL: %s
+           ''' % (rs.id, rs.real_url, rs.fake_url)
+
+app.run(host='0.0.0.0', port=4001)
